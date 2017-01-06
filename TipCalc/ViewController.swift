94e7b9bae29281f7122e7af0801819a21ba8a4ee
Nisarga Patel <np8@umbc.edu>
2017-01-05 23:03:04 -0500

    Added tests and ui tests bundles for future testing

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,55 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // This function calculates a user's tip by taking into
+    // account the amount they put in their Bill text field
+    // and the amount the select on the segmented control
+    // the segmented control will run this function on a
+    // valueChanged event which was connected to this
+    // by right clicking the segmented control and selecting
+    // the valueChanged event
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // There must be an array of values for the tip increments
+        let tipPossibles = [0.18, 0.2, 0.25]
+        
+        // The string value of the billField must be converted
+        // into a numerical double
+        let billInitialValue = Double(billField.text!) ?? 0
+        
+        // Based upon the selected control segment a tip value needs to be chosen
+        let selectedTip = tipPossibles[segmentedControl.selectedSegmentIndex]
+        
+        // The tip value must be calculated
+        let tipValue = billInitialValue * selectedTip
+        
+        // Calculate the total value as the tipValue + billInitialValue
+        let totalValue = billInitialValue + tipValue
+        
+        // Set the text values for the textFields with the correct String formatting i.e. a decimal with 2 decimal places
+        tipLabel.text = String(format: "$%.2f", tipValue)
+        totalLabel.text = String(format: "$%.2f", totalValue)
+        
+    }
+    
+    // This is the value changed function for the segmented value control
+    // When the user changes the value the calculateTip function is automatically run
+    @IBAction func runValueChanged(_ sender: Any) {
+        
+        self.calculateTip(Any.self)
+        
+    }
+    
+    // To get this to show I did Option + Click on the tap gesture recognizer
+    // I then made this an Action with the name
+    // onTap
+    @IBAction func onTap(_ sender: Any) {
+        
+        // This will make the keyboard go away
+        view.endEditing(true)
+        
     }
 
 
